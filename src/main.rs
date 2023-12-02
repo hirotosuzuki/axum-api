55143ab86a10247a5f92bc8034b6e3bb51f31e79
鈴木太郎 <suzukitarou@suzukitarounoMacBook-Pro.local>
2023-12-02 18:04:46 +0900

    sqliteのtodoテーブルをCRUD操作するエンドポイントを実装

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
+mod entity;
+
+use std::sync::Arc;
+
 use axum::{
+    extract::State,
     http::StatusCode,
     response::IntoResponse,
     routing::{get, patch},
     Json, Router,
 };
-use serde_json::json;
+use entity::prelude::{Todo, TodoActiveModel};
+use sea_orm::{ActiveModelTrait, ActiveValue, Database, DatabaseConnection, EntityTrait, Set};
 
 #[tokio::main]
 async fn main() {
+    let db = Database::connect("sqlite://sample.db").await.unwrap();
+    let state = Arc::new(AppState { db });
+
     // curl localhost:3000/api/v1/todos | jq .
     // curl -X POST localhost:3000/api/v1/todos | jq .
     // curl -X PATCH localhost:3000/api/v1/todos/1 | jq .
     // curl -X DELETE localhost:3000/api/v1/todos/1 | jq .
     let api = Router::new()
         .route("/todos", get(todos_index).post(todos_create))
-        .route("/todos/:id", patch(todos_update).delete(todos_delete));
+        .route("/todos/:id", patch(todos_update).delete(todos_delete))
+        .with_state(state);
+
     let app = Router::new().nest("/api/v1", api);
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
@@ -22,34 +33,42 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn todos_index() -> impl IntoResponse {
-    let todo1 = json!({
-        "id": 1,
-        "name": "todo1",
-    });
-    let todo2 = json!({
-        "id": 2,
-        "name": "todo2",
-    });
-    Json(vec![todo1, todo2])
+async fn todos_index(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let todos = Todo::find().all(&state.db).await.unwrap();
+    Json(todos)
 }
 
-async fn todos_create() -> impl IntoResponse {
-    let todo = json!({
-        "id": 1,
-        "name": "todo1",
-    });
-    (StatusCode::CREATED, Json(todo))
+async fn todos_create(State(state): State<Arc<AppState>>) -> Result<impl IntoResponse, StatusCode> {
+    let todo = TodoActiveModel {
+        id: ActiveValue::NotSet,
+        name: ActiveValue::Set("created todo".to_owned()),
+    };
+    todo.insert(&state.db).await.unwrap();
+    Ok(StatusCode::CREATED)
 }
 
-async fn todos_update() -> Result<impl IntoResponse, StatusCode> {
-    let todo = json!({
-        "id": 1,
-        "name": "todo1",
-    });
+// https://www.sea-ql.org/SeaORM/docs/basic-crud/update/
+async fn todos_update(State(state): State<Arc<AppState>>) -> Result<impl IntoResponse, StatusCode> {
+    let mut target: TodoActiveModel = Todo::find_by_id(2)
+        .one(&state.db)
+        .await
+        .unwrap()
+        .unwrap()
+        .into();
+    target.name = Set("updated todo".to_owned());
+    let todo = target.update(&state.db).await.unwrap();
     Ok(Json(todo))
 }
 
-async fn todos_delete() -> impl IntoResponse {
-    (StatusCode::NO_CONTENT, "")
+// https://www.sea-ql.org/SeaORM/docs/basic-crud/delete/
+async fn todos_delete(State(state): State<Arc<AppState>>) -> impl IntoResponse {
+    let response = Todo::delete_by_id(1).exec(&state.db).await.unwrap();
+    if response.rows_affected == 0 {
+        return StatusCode::NOT_FOUND;
+    }
+    StatusCode::NO_CONTENT
+}
+
+struct AppState {
+    db: DatabaseConnection,
 }
